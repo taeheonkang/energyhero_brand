5236a16a357492a61fada38e61fd633b00c1e6bd
taeheonkang <119912177+taeheonkang@users.noreply.github.com>
2022-12-06 15:18:00 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+AOS.init({
+    duration: 1200
+  })
+
+$(function(){
+    $(".log_btn") .click(function(){
+        $(".log_box") .fadeIn();
+    });
+
+    $(".log a") .click(function(){
+        $(".log_box") .fadeOut();
+    });
+
+    $(".sign_btn") .click(function(){
+        $(".sign_box") .fadeIn();
+    });
+
+    $(".sign a") .click(function(){
+        $(".sign_box") .fadeOut();
+    });
+});
